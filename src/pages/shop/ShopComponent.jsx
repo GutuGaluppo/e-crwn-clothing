727d3c4613c11f376515fc790b8edb937cd7eefc
Gutu Galuppo <galuppodev@gmail.com>
2021-03-23 12:05:48 +0100

    created shop folder and implemented shopReducer

--- src/pages/shop/ShopComponent.jsx
+++ src/pages/shop/ShopComponent.jsx
@@ -1,26 +1,25 @@
-import React, { Component } from 'react'
-import SHOP_DATA from './shop.data'
+import React from 'react'
 import CollectionPreview from '../../components/collection/CollectionPreview'
 
-export default class ShopComponent extends Component {
+import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
 
-	constructor(props) {
-		super(props);
-		this.state = {
-			collection: SHOP_DATA,
-		}
-	}
-	render() {
-		const { collection } = this.state
-		return (
-			<div className="shop-page">
-				{
-					collection
-						.map(({ id, ...otherCollectionProps }) => (
-							<CollectionPreview key={id} {...otherCollectionProps} />
-						))
-				}
-			</div>
-		)
-	}
+import { selectCollections } from '../../redux/shop/shop.selector'
+
+const ShopComponent = ({ collection }) => {
+	return (
+		<div className="shop-page">
+			{
+				collection
+					.map(({ id, ...otherCollectionProps }) => (
+						<CollectionPreview key={id} {...otherCollectionProps} />
+					))
+			}
+		</div>
+	)
 }
+
+const mapStateToProps = createStructuredSelector({
+	collection: selectCollections
+})
+export default connect(mapStateToProps)(ShopComponent)
