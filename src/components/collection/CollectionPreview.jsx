b1b2dfcef7869465ba8fbd5c2162140587456962
Gutu Galuppo <galuppodev@gmail.com>
2021-04-01 10:26:59 +0200

    added withRouter onto CollectionPreview

--- src/components/collection/CollectionPreview.jsx
+++ src/components/collection/CollectionPreview.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import { withRouter } from 'react-router-dom'
 import CollectionItem from '../collectionItem/CollectionItem'
 
 import { CollectionPreviewContainer, PreviewDiv } from './collecttion-preview.styles'
 
-const CollectionPreview = ({ title, items }) => {
+const CollectionPreview = ({ title, items, history, match, routeName }) => {
+
 	return (
 		<CollectionPreviewContainer>
-			<h1>{title.toUpperCase()}</h1>
+			<h1 onClick={() => history.push(`${match.path}/${routeName}`)}>
+				{title.toUpperCase()}
+			</h1>
 			<PreviewDiv>
 				{items
 					.filter((item, idx) => idx < 4) // filtering to display only 4 items
@@ -14,8 +18,9 @@ const CollectionPreview = ({ title, items }) => {
 						<CollectionItem key={item.id} item={item} />
 					))}
 			</PreviewDiv>
-		</CollectionPreviewContainer>
+		</CollectionPreviewContainer >
 	)
+	
 }
 
-export default CollectionPreview
+export default withRouter(CollectionPreview)
